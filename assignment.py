4a22b60bcbbd7fe8fdf742a8f48a0f23e0a450b4
FelixKihima <kihimafelix75@gmail.com>
2024-09-05 16:19:54 -0700

    Answered assignment week 2

---
+++ assignment.py
@@ -0,0 +1,25 @@
+
+my_list = []
+print(my_list)
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print("after append:", my_list)
+print(my_list)
+
+
+my_list[1] = 15
+print(my_list)
+
+listend = [50, 60, 70]
+my_list.extend(listend)
+print("list after append:", my_list)
+
+del my_list [6]
+print(my_list)
+
+my_list.sort()
+print(my_list)
+
